88e0232bf5acd4a8c34f825ec8771a4e24bd5e52
rubendroogh <rubendroogh@hotmail.nl>
2019-09-05 23:49:16 +0200

    Created resource controllers.

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Report;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+
+    public function index()
+    {
+        return response()->json(Report::all());
+    }
+
+    public function read($id)
+    {
+        return response()->json(Report::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $report = Report::create($request->all());
+
+        return response()->json($report, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $report = Report::findOrFail($id);
+        $report->update($request->all());
+
+        return response()->json($report, 200);
+    }
+
+    public function delete($id)
+    {
+        Report::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}
---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Template;
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+
+    public function index()
+    {
+        return response()->json(Template::all());
+    }
+
+    public function read($id)
+    {
+        return response()->json(Template::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $template = Template::create($request->all());
+
+        return response()->json($template, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $template = Template::findOrFail($id);
+        $template->update($request->all());
+
+        return response()->json($template, 200);
+    }
+
+    public function delete($id)
+    {
+        Template::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}
